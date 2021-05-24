1f7e2cd8ddaf11418189e995d0a5fd67cc48d041
Modestos Kaffes <modkaffes@gmail.com>
2021-05-24 17:00:25 +0300

    fix: Change handleClickOutside types

--- hooks/useDetectClickOutside.tsx
+++ hooks/useDetectClickOutside.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function useDetectClickOutside(initState: boolean) {
   const triggerRef = useRef<HTMLInputElement>(null); // The element we interacted on (eg, button)
@@ -6,14 +6,14 @@ export default function useDetectClickOutside(initState: boolean) {
 
   const [show, setShow] = useState(initState);
 
-  const handleClickOutside = ({ target }: MouseEvent) => {
+  const handleClickOutside = (event: Event) => {
     // If click is on trigger element, toggle dropdown
-    if (triggerRef.current && triggerRef.current.contains(target as Node)) {
+    if (triggerRef.current && triggerRef.current.contains(event.target as HTMLButtonElement)) {
       return setShow(!show);
     }
 
     // If dropdown is open and click is outside dropdown, close it
-    if (nodeRef.current && !nodeRef.current.contains(target as Node)) {
+    if (nodeRef.current && !nodeRef.current.contains(event.target as HTMLButtonElement)) {
       return setShow(false);
     }
   };
